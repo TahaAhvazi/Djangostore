d520f920a0dd39580921e2451bd87a6e7231a0fc
Taha <tahax.dev@gmail.com>
2022-08-09 21:44:54 +0430

    Work with model serializers

--- store/serializers.py
+++ store/serializers.py
@@ -1,12 +1,25 @@
 from rest_framework import serializers
 
-from store.models import Product
+from store.models import Product, Collection
 from decimal import Decimal
-class ProductSerializer(serializers.Serializer):
+
+
+class CollectionSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     title = serializers.CharField(max_length=255)
-    price = serializers.DecimalField(max_digits=6, decimal_places=2, source = 'unit_price')
-    price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'unit_price', 'collection']
+    # id = serializers.IntegerField()
+    # title = serializers.CharField(max_length=255)
+    # price = serializers.DecimalField(max_digits=6, decimal_places=2, source = 'unit_price')
+    # price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
+    # collection = serializers.HyperlinkedRelatedField(
+    #     queryset = Collection.objects.all(),
+    #     view_name='collection-detail'
+    # )
 
     def calculate_tax(self, product:Product):
         return product.unit_price *Decimal(1.1)
--- store/views.py
+++ store/views.py
@@ -10,8 +10,8 @@ from rest_framework import status
 
 @api_view()
 def product_list(request):
-    queryset = Product.objects.all()
-    serializer = ProductSerializer(queryset, many=True)
+    queryset = Product.objects.select_related('collection').all()
+    serializer = ProductSerializer(queryset, many=True, context={'request':request})
     return Response(serializer.data)
 
 @api_view()
@@ -19,3 +19,6 @@ def product_detail(request, id):
     product = get_object_or_404(Product, pk=id)
     serializer = ProductSerializer(product)
     return Response(serializer.data)
+@api_view()
+def collection_detail(request, pk):
+    return Response('Ok')
