1c0c1793400e17965ac3ec3b16fa76ec9ff64226
Taha <tahax.dev@gmail.com>
2022-08-09 21:48:00 +0430

    Create a function to model serializer

--- store/serializers.py
+++ store/serializers.py
@@ -16,10 +16,10 @@ class ProductSerializer(serializers.ModelSerializer):
     # title = serializers.CharField(max_length=255)
     # price = serializers.DecimalField(max_digits=6, decimal_places=2, source = 'unit_price')
     # price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
-    # collection = serializers.HyperlinkedRelatedField(
-    #     queryset = Collection.objects.all(),
-    #     view_name='collection-detail'
-    # )
+    collection = serializers.HyperlinkedRelatedField(
+        queryset = Collection.objects.all(),
+        view_name='collection-detail'
+    )
 
     def calculate_tax(self, product:Product):
         return product.unit_price *Decimal(1.1)
