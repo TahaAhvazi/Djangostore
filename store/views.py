ef13c0499e174308a964ddd5b95b0f8cbea227f5
Taha <tahax.dev@gmail.com>
2022-08-09 23:31:28 +0430

    Delete a product with serializer

--- store/views.py
+++ store/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .models import Product
+from .models import OrderItem, Product
 from .serializers import ProductSerializer
 from rest_framework import status
 # Create your views here.
@@ -21,7 +21,7 @@ def product_list(request):
         serializer.validated_data
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-@api_view(['GET', 'PUT'])
+@api_view(['GET', 'PUT', 'DELETE'])
 def product_detail(request, id):
     product = get_object_or_404(Product, pk=id)
     if request.method == 'GET':
@@ -31,7 +31,10 @@ def product_detail(request, id):
         serializer = ProductSerializer(product, data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        return Response(serializer.data)
+        return Response(serializer.data) 
+    elif request.method == 'DELETE':
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 @api_view()
 def collection_detail(request, pk):
     return Response('Ok')
