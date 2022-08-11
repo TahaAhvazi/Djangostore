ab62f6b4c09a70732d12b896ee6f77278f83e356
Taha <tahax.dev@gmail.com>
2022-08-11 16:07:12 +0430

    Use ModelViewSet

--- store/views.py
+++ store/views.py
@@ -9,45 +9,28 @@ from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.generics import ListCreateAPIView, RetrieveDestroyAPIView
 from django.db.models import Count
+from rest_framework.viewsets import ModelViewSet
 # Create your views here.
 
-
-class ProductList(ListCreateAPIView):
+class ProductViewSet(ModelViewSet):
     queryset = Product.objects.select_related('collection').all()
     serializer_class = ProductSerializer
 
     def get_serializer_context(self):
         return {'request': self.request}
 
-
-class ProductDetail(APIView):
-    def get(self, request ,id):
-        product = get_object_or_404(Product, pk=id)
-        serializer = ProductSerializer(product)
-        return Response(serializer.data)
-    def put(self, request):
-        product = get_object_or_404(Product, pk=id)
-        serializer = ProductSerializer(product, data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data) 
     def delete(self, request):
         product = get_object_or_404(Product, pk=id)
         product.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-class CollectionList(ListCreateAPIView):
+class CollectionViewSet(ModelViewSet):
     queryset= Collection.objects.annotate(product_count=Count('product')).all()
     serializer_class = CollectionSerializer
 
-class CollectionDetail(RetrieveDestroyAPIView):
-    queryset = Collection.objects.annotate(products_count=Count('product'))
-    serializer_class=CollectionSerializer
-
     def delete(self, request, pk):
         collection = get_object_or_404(Collection, pk=pk)
-        if collection.product.count()> 0:
-            return Response({'error':'Collection have some products'}, status=status.HTTP_204_NO_CONTENT)
         collection.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
