53ba6374cfe96c0bed96d37ee6224096a9d82fae
Taha <tahax.dev@gmail.com>
2022-08-14 21:10:31 +0430

    Create Custom User

--- storefront/settings.py
+++ storefront/settings.py
@@ -41,9 +41,9 @@ INSTALLED_APPS = [
     'playground',
     'debug_toolbar',
     'store',
-    'store_custom',
     'tags',
     'likes',
+    'core',
     'django_filters',
 ]
 
@@ -145,3 +145,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'COERCE_DECIMAL_TO_STRING': False,
 }
+
+AUTH_USER_MODEL = 'core.User'
